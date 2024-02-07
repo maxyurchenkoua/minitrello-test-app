557dc6e21ad465dc14e8854d7d365e4af2a04d6a
Max Yurchenko <yurchenkomax@inbox.ru>
2024-02-07 09:55:38 +0200

    daisyui tailwind component library install

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ function App() {
   return (
     <div className="App">
       <p className="text-xl font-bold">Blablabala</p>
+      <button className="btn">Hello daisyUI</button>
+      <div className="badge badge-secondary">secondary</div>
     </div>
   );
 }
