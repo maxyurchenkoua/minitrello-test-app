509284db24a06f5e9ed8710c2657959b471daf5d
Max Yurchenko <yurchenkomax@inbox.ru>
2024-02-09 19:29:35 +0200

    loading spinner

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import { useQuery, gql } from "@apollo/client";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
-import BoardSectionList from './components/BoardSectionList';
+import BoardSectionList from "./components/BoardSectionList";
 
+
+// const GET_BOARDS = gql`
+//   query GetBoards {
+//     boards {
+//       id
+//       name
+//       position
+//       tasks {
+//         id
+//         title
+//         description
+//       }
+//     }
+//   }
+// `;
 
 const QUERY = gql`
   {
@@ -18,15 +33,21 @@ const QUERY = gql`
 function App() {
   const { loading, error, data } = useQuery(QUERY);
 
-  if (loading) return <p>Loading...</p>;
+  if (loading) return (
+    <div className="mx-auto mt-48 text-center">
+      <span className="loading loading-spinner loading-lg"></span>
+    </div>
+  )
   if (error) return <p>Error</p>;
-
   return (
     <div className="App">
       <Header />
       <div className="drawer lg:drawer-open">
         <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
         <div className="drawer-content bg-slate-200">
+          {/* {JSON.stringify(data.boards)} */}
+          {/* <BoardSectionList boards={data.boards.tasks} /> */}
+
           <BoardSectionList tasks={data.tasks} />
         </div>
         <div className="drawer-side">
