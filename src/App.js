7056294500ecb4990cd2f03c14c779e624d6b719
Max Yurchenko <yurchenkomax@inbox.ru>
2024-02-09 10:58:28 +0200

    basic drag-n-drop finctionality

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
-import Dashboard from "./components/Dashboard";
 import Sidebar from "./components/Sidebar";
+
+import BoardSectionList from './components/BoardSectionList';
 
 function App() {
   return (
@@ -8,8 +9,8 @@ function App() {
       <Header />
       <div className="drawer lg:drawer-open">
         <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-        <div className="drawer-content flex flex-col items-center justify-center bg-gray-50">
-          <Dashboard />
+        <div className="drawer-content bg-slate-200">
+          <BoardSectionList />
         </div>
         <div className="drawer-side">
           <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay"></label>
---
+++ src/data/index.js
@@ -0,0 +1,32 @@
+export const INITIAL_TASKS = [
+  {
+    id: 123,
+    title: 'Title 2',
+    description: 'Desc 2',
+    status: 'todo',
+  },
+  {
+    id: 321,
+    title: 'Title 3',
+    description: 'Desc 3',
+    status: 'todo',
+  },
+  {
+    id: 2332,
+    title: 'Title 4',
+    description: 'Desc 4',
+    status: 'done',
+  },
+  {
+    id: 23327,
+    title: 'Title 5',
+    description: 'Desc 554',
+    status: 'in progress',
+  },
+  {
+    id: 4343243,
+    title: 'in blablabla',
+    description: 'Desc 554',
+    status: 'in blablabla',
+  },
+];
